d08805572955b4d8b99d13458d39d3b2b27caae0
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-07-11 13:29:36 -0400

    Added main and basic enumeration

--- src/textnode.py
+++ src/textnode.py
@@ -1,11 +1,26 @@
-from enum import enum
+from enum import Enum
 
 class TextType(Enum):
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
-class TextNode(TEXT, TEXT_TYPE, URL):
-    
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, otherNode):
+        return (
+            self.text == otherNode.text,
+            self.text_type == otherNode.text_type,
+            self.url == otherNode.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url}"
+
