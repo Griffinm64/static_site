fdcaa76ccf849e899ff73a5d87cc97c2f08e54d1
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-07-13 06:21:06 -0400

    Added HTMLNode and unit test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextType, TextNode
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+    node = TextNode("This is some anchor text", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
 if __name__ == main():
