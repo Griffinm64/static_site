75187a6f66d7dcf21e9ca2b04f7dd66fb123ad98
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-07-17 02:48:04 -0400

    prepared for transition to github pages

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
+basepath = sys.argv[1]
 
-dir_path_static = "./static"
-dir_path_public = "./public"
-dir_path_content = "./content"
-template_path = "./template.html"
+dir_path_static = f"{basepath}/static"
+dir_path_public = f"{basepath}/docs"
+dir_path_content = f"{basepath}/content"
+template_path = f"{basepath}/template.html"
+
 
 
 def main():
